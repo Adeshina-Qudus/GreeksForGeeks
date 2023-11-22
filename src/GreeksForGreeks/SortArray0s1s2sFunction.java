2e523e72af7697a2383ac0d281564cebeac09099
Adeshina Qudus <qudusa55@gmail.com>
2023-11-22 09:41:45 +0100

    another commit

---
+++ src/GreeksForGreeks/SortArray0s1s2sFunction.java
@@ -0,0 +1,16 @@
+package GreeksForGreeks;
+
+public class SortArray0s1s2sFunction {
+    public static int[] sortArray(int[] numbers){
+        for (int outter = 0 ; outter < numbers.length; outter++){
+            for (int inner = 0; inner < numbers.length; inner++){
+                if (numbers[outter] < numbers[inner]){
+                    int temp = numbers[inner];
+                    numbers[inner] = numbers[outter];
+                    numbers[outter] = temp;
+                }
+            }
+        }
+        return numbers;
+    }
+}
---
+++ src/GreeksForGreeks/SortArray0s1s2sMain.java
@@ -0,0 +1,13 @@
+package GreeksForGreeks;
+
+
+import java.util.Arrays;
+import static GreeksForGreeks.SortArray0s1s2sFunction.sortArray;
+
+public class SortArray0s1s2sMain {
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(sortArray(new int[]{0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1})));
+    }
+}
