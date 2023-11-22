5ee3ef66050cf57c8e2b47c2f184bff984c70ccf
Adeshina Qudus <qudusa55@gmail.com>
2023-11-22 05:15:39 +0100

    first commit

---
+++ src/GreeksForGreeks/CountNumberOfOccurrencesFunction.java
@@ -0,0 +1,16 @@
+package GreeksForGreeks;
+
+
+    public class CountNumberOfOccurrencesFunction {
+        public static void occurrences(int[] numbers, int occurrencesNumber) {
+
+            int counter = 0;
+            for (int count = 0; count < numbers.length; count++) {
+                if (numbers[count] == occurrencesNumber) {
+                    counter++;
+                }
+            }
+            System.out.println(occurrencesNumber + " occurs " + counter + " times");
+        }
+    }
+
---
+++ src/GreeksForGreeks/CountNumberOfOccurrencesMain.java
@@ -0,0 +1,11 @@
+package GreeksForGreeks;
+
+
+import static GreeksForGreeks.CountNumberOfOccurrencesFunction.occurrences;
+
+public class CountNumberOfOccurrencesMain {
+    public static void main(String[] args) {
+        occurrences(new int[]{1, 1, 2, 2, 2, 2, 3,},2);
+
+    }
+}
